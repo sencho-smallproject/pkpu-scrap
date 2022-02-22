91c5011bc923d20830ded656afb7a60e818e9ff2
sencho <senchoparameswara@gmail.com>
2022-02-22 19:38:34 +0700

    new code

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get('http://jadwalsholat.pkpu.or.id/?id=308')
+soup = BeautifulSoup(req.text, 'html.parser')
+
+scrap = soup.find_all('tr', 'table_highlight')
+scrap = scrap[0]
+
+data = {}
+
+datt = soup.find('td', {'align': 'center'}).find('b').text
+place = soup.find('td', {'align': 'center'}).find('small').find('b').text
+
+i = 0
+for j in scrap:
+    if i > 0:
+        if i == 1:
+            data['subuh'] = j.get_text()
+        elif i == 2:
+            data['dzuhur'] = j.get_text()
+        elif i == 3:
+            data['ashar'] = j.get_text()
+        elif i == 4:
+            data['maghrib'] = j.get_text()
+        elif i == 5:
+            data['isya'] = j.get_text()
+    else:
+        data['date'] = j.get_text() + ' ' + datt + ' di ' + place
+    i =+ 1
+
+print(f"jadwal sholat pada tanggal {data['date']}, yaitu:")
+print(f"subuh: {data['subuh']}")
+print(f"dzuhur: {data['dzuhur']}")
+print(f"ashar: {data['ashar']}")
+print(f"maghrib: {data['maghrib']}")
+print(f"isya: {data['isya']}")
+
